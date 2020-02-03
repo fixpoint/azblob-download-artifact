76f6ce172800e3d3543d1f1916bd6daa2dacb761
lambdalisue <lambdalisue@hashnote.net>
2020-02-03 14:14:36 +0900

    Follow actions/download-artifact spec

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-AZ_CONTAINER=$1; shift
 AZ_NAME=$1; shift
 AZ_PATH=$1; shift
 
-if [[ -z "$AZ_PATH" ]]; then
-  AZ_PATH="$AZ_NAME"
-fi
-
-# Download a specified file
-az storage blob download -c "$AZ_CONTAINER" -n "$AZ_NAME" -f "$AZ_PATH"
+az storage blob download-batch -d "$AZ_PATH" -s "$AZ_NAME"
