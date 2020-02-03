c80c813c89a8f144caad2d8585b5a6dc79153aeb
lambdalisue <lambdalisue@hashnote.net>
2020-02-03 16:33:10 +0900

    Use top directory name instead of container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+AZ_CONTAINER=$1; shift
 AZ_NAME=$1; shift
 AZ_PATH=$1; shift
 
-mkdir -p "$AZ_PATH"
-az storage blob download-batch -d "$AZ_PATH" -s "$AZ_NAME"
+mkdir -p "$AZ_PATH/$AZ_NAME"
+az storage blob download-batch -d "$AZ_PATH" -s "$AZ_CONTAINER" --pattern "$AZ_NAME/*"
